180b841925343b311f397149b2be36e77e76ca4a
IPMokhov <Jora55524@gmail.com>
2024-12-11 18:28:13 +0300

    Add files via upload

--- module_4_2.py
+++ module_4_2.py
@@ -1,8 +1,7 @@
 def test_function():
-    print('Я в области видимости функции test_function')
     def inner_function():
         print('Я в области видимости функции test_function')
-        inner_function()
+    inner_function()
 
 
 test_function() #inner_function() выдает ошибку
